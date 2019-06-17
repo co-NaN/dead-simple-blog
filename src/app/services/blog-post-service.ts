db58102d9651f9b61cac5c8a5da093f74606bf3c
polya.daniel <polya.daniel@dss.hu>
2019-06-17 15:26:49 +0200

    Added the skeleton for retrieving a saved blog post.

--- src/app/services/blog-post-service.ts
+++ src/app/services/blog-post-service.ts
@@ -1,10 +1,15 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {BlogPost} from '../model/blog-post';
+import {Observable} from 'rxjs';
 
 @Injectable()
 export class BlogPostService {
   constructor(private http: HttpClient) {}
+
+  getBlogPostById(id: number): Observable<BlogPost> {
+    return this.http.get<BlogPost>('/api/blog/post/${id}');
+  }
 
   saveBlogPost(blogPost: BlogPost) {
     return this.http.post('/api/blog/post', blogPost);
