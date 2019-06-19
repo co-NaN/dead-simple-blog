280c17efc10cd274663aa437ef9521524cec6b63
polya.daniel <polya.daniel@dss.hu>
2019-06-19 16:53:44 +0200

    Added better logs.

--- server/get-blog-posts.route.ts
+++ server/get-blog-posts.route.ts
@@ -2,7 +2,7 @@ import {Request, Response} from 'express';
 
 export function getAllBlogPosts(req: Request, res: Response) {
 
-  console.log('Retrieving blog posts ...');
+  console.log('Retrieving all blog posts from DB...');
 
   res.status(200).json({
     payload: [{0: 'test'}]
--- server/save-blog-post.route.ts
+++ server/save-blog-post.route.ts
@@ -2,7 +2,7 @@ import {Request, Response} from 'express';
 
 export function saveBlogPost(req: Request, res: Response) {
 
-  console.log('Saving blog post...');
+  console.log('Saving blog post to DB...');
 
   res.status(200).json(
     {payload: 'test save success'}
